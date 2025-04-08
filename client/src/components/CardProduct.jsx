87f28b7e82b8a2b6a75e24ceb48b92cec28cd2c6
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2025-04-09 01:27:04 +0530

    updated code

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -24,9 +24,9 @@ const CardProduct = ({data}) => {
             />
       </div>
       <div className='flex items-center gap-1'>
-        <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50'>
+        {/* <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50'>
               10 min 
-        </div>
+        </div> */}
         <div>
             {
               Boolean(data.discount) && (
